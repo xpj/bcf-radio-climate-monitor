e4413175fcb39f0e3b6ad36d82cbcf356356e25f
xpj <xpj@xpj.cz>
2019-01-18 19:46:16 +0100

    add reset every 1 hour and after failed publish and

--- app/application.c
+++ app/application.c
@@ -1,12 +1,10 @@
 #include <application.h>
 
-#define SERVICE_INTERVAL_INTERVAL (60 * 60 * 1000)
-#define BATTERY_UPDATE_INTERVAL   (60 * 60 * 1000)
+#define AUTOMATIC_RESTART_INTERVAL (60 * 60 * 1000) // 1 hour
+#define BATTERY_UPDATE_INTERVAL   (5 * 60 * 1000) // 5 min
 
-#define UPDATE_SERVICE_INTERVAL            (5 * 1000)
-#define UPDATE_NORMAL_INTERVAL             (10 * 1000)
-#define BAROMETER_UPDATE_SERVICE_INTERVAL  (1 * 60 * 1000)
-#define BAROMETER_UPDATE_NORMAL_INTERVAL   (5 * 60 * 1000)
+#define UPDATE_NORMAL_INTERVAL             (10 * 1000) // 10 sec
+#define BAROMETER_UPDATE_NORMAL_INTERVAL   (1 * 60 * 1000) // 1 min
 
 #define TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL (15 * 60 * 1000)
 #define TEMPERATURE_TAG_PUB_VALUE_CHANGE 0.2f
@@ -34,19 +32,10 @@ bc_led_t led;
 // Thermometer instance
 bc_tmp112_t tmp112;
 
-// Button instance
-bc_button_t button;
-uint16_t button_event_count = 0;
-
-void button_event_handler(bc_button_t *self, bc_button_event_t event, void *event_param)
+void restart()
 {
-    (void) self;
-    (void) event_param;
-
-    if (event == BC_BUTTON_EVENT_PRESS)
-    {
-        bc_led_pulse(&led, 100);
-    }
+    bc_log_info("reset required");
+    bc_system_reset();
 }
 
 void battery_event_handler(bc_module_battery_event_t event, void *event_param)
@@ -57,9 +46,12 @@ void battery_event_handler(bc_module_battery_event_t event, void *event_param)
 
     if (event == BC_MODULE_BATTERY_EVENT_UPDATE)
     {
+        bc_log_info("sending battery event");
         if (bc_module_battery_get_voltage(&voltage))
         {
-            bc_radio_pub_battery(&voltage);
+            if (!bc_radio_pub_battery(&voltage)) {
+                restart();
+            }
         }
     }
 }
@@ -73,11 +65,16 @@ void climate_module_event_handler(bc_module_climate_event_t event, void *event_p
 
     if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_THERMOMETER)
     {
+        bc_log_info("entered thermometer section");
         if (bc_module_climate_get_temperature_celsius(&value))
         {
             if ((fabs(value - params.temperature.value) >= TEMPERATURE_TAG_PUB_VALUE_CHANGE) || (params.temperature.next_pub < bc_scheduler_get_spin_tick()))
             {
-                bc_radio_pub_temperature(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
+                bc_log_info("sending thermometer event");
+                if (!bc_radio_pub_temperature(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value))
+                {
+                    restart();
+                }
                 params.temperature.value = value;
                 params.temperature.next_pub = bc_scheduler_get_spin_tick() + TEMPERATURE_TAG_PUB_NO_CHANGE_INTEVAL;
             }
@@ -85,11 +82,16 @@ void climate_module_event_handler(bc_module_climate_event_t event, void *event_p
     }
     else if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_HYGROMETER)
     {
+        bc_log_info("entered hygrometer section");
         if (bc_module_climate_get_humidity_percentage(&value))
         {
             if ((fabs(value - params.humidity.value) >= HUMIDITY_TAG_PUB_VALUE_CHANGE) || (params.humidity.next_pub < bc_scheduler_get_spin_tick()))
             {
-                bc_radio_pub_humidity(BC_RADIO_PUB_CHANNEL_R3_I2C0_ADDRESS_DEFAULT, &value);
+                bc_log_info("sending hygrometer event");
+                if (!bc_radio_pub_humidity(BC_RADIO_PUB_CHANNEL_R3_I2C0_ADDRESS_DEFAULT, &value))
+                {
+                    restart();
+                }
                 params.humidity.value = value;
                 params.humidity.next_pub = bc_scheduler_get_spin_tick() + HUMIDITY_TAG_PUB_NO_CHANGE_INTEVAL;
             }
@@ -97,16 +99,21 @@ void climate_module_event_handler(bc_module_climate_event_t event, void *event_p
     }
     else if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_LUX_METER)
     {
+        bc_log_info("entered lux meter section");
         if (bc_module_climate_get_illuminance_lux(&value))
         {
             if (value < 1)
             {
                 value = 0;
             }
+            bc_log_info("sending lux meter event");
             if ((fabs(value - params.illuminance.value) >= LUX_METER_TAG_PUB_VALUE_CHANGE) || (params.illuminance.next_pub < bc_scheduler_get_spin_tick()) ||
                     ((value == 0) && (params.illuminance.value != 0)) || ((value > 1) && (params.illuminance.value == 0)))
             {
-                bc_radio_pub_luminosity(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value);
+                if (!bc_radio_pub_luminosity(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value))
+                {
+                    restart();
+                }
                 params.illuminance.value = value;
                 params.illuminance.next_pub = bc_scheduler_get_spin_tick() + LUX_METER_TAG_PUB_NO_CHANGE_INTEVAL;
             }
@@ -114,18 +121,23 @@ void climate_module_event_handler(bc_module_climate_event_t event, void *event_p
     }
     else if (event == BC_MODULE_CLIMATE_EVENT_UPDATE_BAROMETER)
     {
+        bc_log_info("entered barometer section");
         if (bc_module_climate_get_pressure_pascal(&value))
         {
             if ((fabs(value - params.pressure.value) >= BAROMETER_TAG_PUB_VALUE_CHANGE) || (params.pressure.next_pub < bc_scheduler_get_spin_tick()))
             {
                 float meter;
 
+                bc_log_info("sending barometer event");
                 if (!bc_module_climate_get_altitude_meter(&meter))
                 {
                     return;
                 }
 
-                bc_radio_pub_barometer(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value, &meter);
+                if (!bc_radio_pub_barometer(BC_RADIO_PUB_CHANNEL_R1_I2C0_ADDRESS_DEFAULT, &value, &meter))
+                {
+                    restart();
+                }
                 params.pressure.value = value;
                 params.pressure.next_pub = bc_scheduler_get_spin_tick() + BAROMETER_TAG_PUB_NO_CHANGE_INTEVAL;
             }
@@ -133,18 +145,12 @@ void climate_module_event_handler(bc_module_climate_event_t event, void *event_p
     }
 }
 
-void switch_to_normal_mode_task(void *param)
-{
-    bc_module_climate_set_update_interval_thermometer(UPDATE_NORMAL_INTERVAL);
-    bc_module_climate_set_update_interval_hygrometer(UPDATE_NORMAL_INTERVAL);
-    bc_module_climate_set_update_interval_lux_meter(UPDATE_NORMAL_INTERVAL);
-    bc_module_climate_set_update_interval_barometer(BAROMETER_UPDATE_SERVICE_INTERVAL);
-
-    bc_scheduler_unregister(bc_scheduler_get_current_task_id());
-}
-
 void application_init(void)
 {
+
+    bc_log_init(BC_LOG_LEVEL_INFO, BC_LOG_TIMESTAMP_ABS);
+    bc_log_info("application_init started");
+
     // Initialize LED
     bc_led_init(&led, BC_GPIO_LED, false, false);
     bc_led_set_mode(&led, BC_LED_MODE_OFF);
@@ -155,10 +161,6 @@ void application_init(void)
     // Initialize radio
     bc_radio_init(BC_RADIO_MODE_NODE_SLEEPING);
 
-    // Initialize button
-    bc_button_init(&button, BC_GPIO_BUTTON, BC_GPIO_PULL_DOWN, false);
-    bc_button_set_event_handler(&button, button_event_handler, &button_event_count);
-
     // Initialize battery
     bc_module_battery_init();
     bc_module_battery_set_event_handler(battery_event_handler, NULL);
@@ -167,15 +169,16 @@ void application_init(void)
     // Initialize climate module
     bc_module_climate_init();
     bc_module_climate_set_event_handler(climate_module_event_handler, NULL);
-    bc_module_climate_set_update_interval_thermometer(UPDATE_SERVICE_INTERVAL);
-    bc_module_climate_set_update_interval_hygrometer(UPDATE_SERVICE_INTERVAL);
-    bc_module_climate_set_update_interval_lux_meter(UPDATE_SERVICE_INTERVAL);
+    bc_module_climate_set_update_interval_thermometer(UPDATE_NORMAL_INTERVAL);
+    bc_module_climate_set_update_interval_hygrometer(UPDATE_NORMAL_INTERVAL);
+    bc_module_climate_set_update_interval_lux_meter(UPDATE_NORMAL_INTERVAL);
     bc_module_climate_set_update_interval_barometer(BAROMETER_UPDATE_NORMAL_INTERVAL);
     bc_module_climate_measure_all_sensors();
 
     bc_radio_pairing_request("climate-monitor", VERSION);
 
-    bc_scheduler_register(switch_to_normal_mode_task, NULL, SERVICE_INTERVAL_INTERVAL);
+    bc_scheduler_register(restart, NULL, AUTOMATIC_RESTART_INTERVAL);
 
     bc_led_pulse(&led, 2000);
+    bc_log_info("application_init done");
 }
